e4facf72402ce1af74637b4e1126954fa9d52651
Adonis <39387670+Ad0ns@users.noreply.github.com>
2024-12-22 04:03:10 +0200

    Add files via upload

---
+++ airBnB/index.js
@@ -0,0 +1,27 @@
+
+
+
+new Swiper('.card-wrapper', {
+    loop: true,
+    spaceBetween: 30,
+  
+
+
+
+  breakpoints : {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+    
+  },
+
+ 
+}
+
+);
